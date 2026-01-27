bcd9acac23364b1e3b98e8df978a83d3545db930
Mohanad Abdallah <mohanad1967@yahoo.com>
2026-01-27 14:53:36 +0200

    changing app icon

--- lib/core/utils/manager/assets_manager/image_manager.dart
+++ lib/core/utils/manager/assets_manager/image_manager.dart
@@ -1,5 +1,5 @@
 class ImageManager {
-  static const String onboardingImage = 'assets/images/Onboarding.png';
+  static const String onboardingImage = 'assets/images/building1.jpg';
   static const String flatImage = 'assets/images/flat_image.png';
   static const String errorImage = 'assets/images/Error_image.png';
   static const String loadingImage = 'assets/images/loadinag_image.png';
